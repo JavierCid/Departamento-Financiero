2130398f07824e487f1f74b844090eb198f83b9a
javier.cid <javier.cid@momentumreim.com>
2025-10-28 14:28:35 +0100

    compaquetado

--- Program.cs
+++ Program.cs
@@ -2,9 +2,16 @@ using Departamento_Financiero;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+public partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
