6bce4d86c752ffcf8ac2d9fd62bd04d8a5b6fe29
javier.cid <javier.cid@momentumreim.com>
2025-11-24 16:52:48 +0100

    s

--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(sp => new System.Net.Http.HttpClient());
+
 
 var app = builder.Build();
 
