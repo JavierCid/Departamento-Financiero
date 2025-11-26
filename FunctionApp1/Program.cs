8a98c52f7e346351045196ad2cf449274fc50aa9
javier.cid <javier.cid@momentumreim.com>
2025-11-26 10:51:22 +0100

    ready y ya con contraste contabilidad prinex

---
+++ FunctionApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+
+namespace FunctionApp1
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
